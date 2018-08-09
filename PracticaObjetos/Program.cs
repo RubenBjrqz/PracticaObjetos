f1db00a2867a6a416cff9f1a7be88cbba1532ce5
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-08-09 08:42:28 -0700

    Agregar archivos de proyecto.

---
+++ PracticaObjetos/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrácticaObjetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona1 = new Persona();
+        }
+    }
+}
