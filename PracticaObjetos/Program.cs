040775b18c5500ba4f0c86ca2e966f1b68650927
Rubén Bojórquez <rubenbjrqz@outlook.es>
2018-08-16 08:33:44 -0700

    En este cambio icimos una clase con constructores

--- PracticaObjetos/Program.cs
+++ PracticaObjetos/Program.cs
@@ -10,25 +10,31 @@ namespace PrácticaObjetos
     {
         static void Main(string[] args)
         {
-            //Objeto es el espacio de memoria o ram que ocupa
-            //Clase es un modelo que define un conjunto de variables
-            //Public es un modificador de acceso
+            Contacto contacto1 = new Contacto();
 
-            Persona persona1 = new Persona();
-            persona1.nombre = "José";
-            persona1.apellidos = "José";
-            persona1.edad = 70;
-            persona1.altura = 1.5f;
-            Console.WriteLine(persona1.nombre + " " + persona1.apellidos + " tiene " + persona1.edad + " Años");
+            contacto1.Nombre = "Benito Camela";
+            contacto1.TelefonoCasa = "(644) 333 - 5324";
+            contacto1.Direccion = "Demiano 523 ";
 
-            persona1.cumplirAños();
-            Console.WriteLine("Ahora " + persona1.nombre + " tiene " + persona1.edad + " Años");
 
-            Console.WriteLine(persona1.nombre + " mide: " + persona1.altura);
-            persona1.crecer(0.12f);
-            Console.WriteLine(persona1.nombre + " ahora mide: " + persona1.altura);
+            Console.WriteLine("Nombre: " + contacto1.Nombre);
+            Console.WriteLine("Telefono de casa: " + contacto1.TelefonoCasa);
+
+            Contacto contacto2 = new Contacto("Batman");
+            Console.WriteLine("Contacto 2");
+            Console.WriteLine("Nombre: " + contacto2.Nombre);
+
+            Contacto Contacto3 = new Contacto("Doña Mary", "34645435", "4134134", "Calle Chihuahua", "Ladoñamary@gmail.com");
+
+            Console.WriteLine("Contacto 3");
+            Console.WriteLine("Nombre:" + Contacto3.Nombre);
+            Console.WriteLine("Celular: " + Contacto3.TelefonoCelular);
+            Console.WriteLine("Telefono Casa: " + Contacto3.TelefonoCasa);
+            Console.WriteLine("Direccion: " + Contacto3.Direccion);
+            Console.WriteLine("E-mail: " + Contacto3.CorreoElectronico);
 
             Console.ReadLine();
+
         }
     }
 }
